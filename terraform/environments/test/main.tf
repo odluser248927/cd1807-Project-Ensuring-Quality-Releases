26f59cf055930f305cda2a78307ddb71c80ef2ea
odluserDGray <odluser248927@gmail.com>
2024-02-20 03:07:01 +0000

    working always

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
-  # skip_provider_registration = "true"
+  skip_provider_registration = "true"
   # version = "1.5.7"
   features {}
 }
