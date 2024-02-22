4f1a8800433ba29f1d39efbce372d59bdf8b3f0b
odluserDGray <odluser248927@gmail.com>
2024-02-22 04:06:36 +0000

    new pipeLine deployment

--- terraform/environments/test/backend.tf
+++ terraform/environments/test/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-   backend "azurerm" {    
-     resource_group_name  = "Azuredevops"
-     storage_account_name = "myazuredevopsdemoblob"
-     container_name       = "tfstate"
-     key                  = "terraform.tfstate"
-     access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
-   }    
-}
+# terraform {
+#    backend "azurerm" {    
+#      resource_group_name  = "Azuredevops"
+#      storage_account_name = "myazuredevopsdemoblob"
+#      container_name       = "tfstate"
+#      key                  = "terraform.tfstate"
+#      access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
+#    }    
+# }
