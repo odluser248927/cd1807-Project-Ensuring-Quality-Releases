82614211192e33e543de9701171a6a0d00ebcb93
odluserDGray <odluser248927@gmail.com>
2024-02-22 04:23:42 +0000

    new pipeLine deployment

--- terraform/environments/test/backend.tf
+++ terraform/environments/test/backend.tf
@@ -18,8 +18,8 @@ terraform {
      access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
    }
 
-  provider "azurerm" {
-  subscription_id = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
-    features {}
-  }
+  # provider "azurerm" {
+  # subscription_id = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
+  #   features {}
+  # }
 }
