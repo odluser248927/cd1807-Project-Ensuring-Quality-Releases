6e0ab9cfea6721c47948d27e98193d3f25b27183
odluserDGray <odluser248927@gmail.com>
2024-02-22 04:28:09 +0000

    new pipeLine deployment

--- terraform/environments/test/backend.tf
+++ terraform/environments/test/backend.tf
@@ -10,13 +10,13 @@ terraform {
     }
   }
 }  
-   backend "azurerm" {    
-     resource_group_name  = "Azuredevops"
-     storage_account_name = "myazuredevopsdemoblob"
-     container_name       = "tfstate"
-     key                  = "terraform.tfstate"
-     access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
-   }
+   # backend "azurerm" {    
+   #   resource_group_name  = "Azuredevops"
+   #   storage_account_name = "myazuredevopsdemoblob"
+   #   container_name       = "tfstate"
+   #   key                  = "terraform.tfstate"
+   #   access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
+   # }
 
   # provider "azurerm" {
   # subscription_id = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
