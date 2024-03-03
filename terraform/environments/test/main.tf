6a9e5d74697336165f58b12d8b8b1bbbc1006948
odluserDGray <odluser248927@gmail.com>
2024-03-03 04:49:45 +0000

    updated the contributor role, testing deployment.

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -38,14 +38,14 @@ provider "azurerm" {
 #   }
 # }
 
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name = ""
-#    storage_account_name = "" 
-#    container_name       = "" 
-#    key                  = ""  
-#  }
-#}
+terraform {
+ backend "azurerm" {
+   resource_group_name = ""
+   storage_account_name = "" 
+   container_name       = "" 
+   key                  = ""  
+ }
+}
 
 module "resource_group" {
   source               = "../../modules/resource_group"
