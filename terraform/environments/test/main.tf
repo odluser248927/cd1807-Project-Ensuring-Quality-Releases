49e1e8a2aeb55f342c5b15f3a3245a4c941783c0
odluserDGray <odluser248927@gmail.com>
2024-03-05 03:16:08 +0000

    updated the contributor role, testing deployment.

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -9,15 +9,15 @@ provider "azurerm" {
   features {}
 }
 
-import {
-  id = "subnet_id=/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops/providers/Microsoft.Network/virtualNetworks/mydevopsagentvm-vnet/subnets/default"
-  to = module.network.azurerm_virtual_network.Azuredevops
- }
+# import {
+#   id = "subnet_id=/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops/providers/Microsoft.Network/virtualNetworks/mydevopsagentvm-vnet/subnets/default"
+#   to = module.network.azurerm_virtual_network.Azuredevops
+#  }
 
-import {
-  id = "resource_group=/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
-  to = module.resource_group.azurerm_resource_group.Azuredevops
-}
+# import {
+#   id = "resource_group=/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
+#   to = module.resource_group.azurerm_resource_group.Azuredevops
+# }
 
  # import {
  #   id = "/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops/providers/providers/Microsoft.Network/networkSecurityGroups/mydevopsagent-nsg"
