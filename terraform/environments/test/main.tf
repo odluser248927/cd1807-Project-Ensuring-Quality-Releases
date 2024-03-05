0f34d5e04f06ffa383e2fa8b43945a11155e5a6b
odluserDGray <odluser248927@gmail.com>
2024-03-05 11:41:08 +0000

    still debugging and testing deployment.

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 #  }
 
 import {
-  id = "resource_group=/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
+  id = "/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
   to = module.resource_group.azurerm_resource_group.Azuredevops
 }
 
