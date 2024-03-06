af72c22ad8d6f867885004973b998a9c7a548c08
odluserDGray <odluser248927@gmail.com>
2024-03-06 03:07:15 +0000

    still debugging and testing deployment.

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
   # subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
-  subscription_id      = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
-  tenant_id            = "6b6c018c-f2e6-48d8-b64f-4cd02807e329"
+  # subscription_id      = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
+  # tenant_id            = "6b6c018c-f2e6-48d8-b64f-4cd02807e329"
   skip_provider_registration = "true"
   # version     = ">= 2.61.0"
   # version = ">=1.5.7"
@@ -37,9 +37,9 @@ terraform {
     #resource_group       = "/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
     #container_name       = "terraform.tfstate"
     container_name       = "tfstate"
-    key                  = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
-    # key                  = "terraform.tfstate"
-    # tenant_id            = "6b6c018c-f2e6-48d8-b64f-4cd02807e329"
+    # key                  = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
+    key                  = "terraform.tfstate"
+    tenant_id            = "6b6c018c-f2e6-48d8-b64f-4cd02807e329"
     access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
   }
 }
