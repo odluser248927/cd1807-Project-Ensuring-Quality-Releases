79b23a8762a4bd9e55776da4250bfa1c62ee6d39
odluserDGray <odluser248927@gmail.com>
2024-03-12 02:27:02 +0000

    testing deployment still..

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -34,12 +34,13 @@ terraform {
   backend "azurerm" {
     storage_account_name = "myazuredevopsdemoblob"
     #resource_group       = "${var.resource_group}"
-    #resource_group       = "/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
+    resource_group       = "/subscriptions/64b5de4c-738a-488c-9d2c-0a702a6d086a/resourceGroups/Azuredevops"
     #container_name       = "terraform.tfstate"
     container_name       = "tfstate"
     # key                  = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
     key                  = "terraform.tfstate"
     tenant_id            = "6b6c018c-f2e6-48d8-b64f-4cd02807e329"
+    subscription_id      = "64b5de4c-738a-488c-9d2c-0a702a6d086a"
     access_key           = "SuLBd1YGQF4aMCukZa7CMlRBKCydv7alo8CMRPJl6N03etepMrNTxGw3MUjw7scLD1r8yG7X42Je+AStVvMVlA=="
   }
 }
