f3c942ba76a6e19af044c0ec453f1275edb942c5
odluserDGray <odluser248927@gmail.com>
2024-12-08 01:13:55 +0000

    updated selenium login with more output for terminal debug.

--- terraform/environments/test/automatedtesting/selenium/login.py
+++ terraform/environments/test/automatedtesting/selenium/login.py
@@ -1,13 +1,12 @@
-#from pathlib import path
-from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+#!/usr/bin/env python3
 
-#options = ChromeOptions()
-driver = webdriver.Chrome()
-driver = webdriver.Chrome('./chromedriver')
-#driver.get("http://www.python.org")
-driver.get("https://www.saucedemo.com/")
+import time
+import os
+import re 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+options = webdriver.ChromeOptions()
 
 # Start the browser and login with standard_user
 def login (user, password):
@@ -15,13 +14,26 @@ def login (user, password):
     # --uncomment when running in Azure DevOps.
     #options = ChromeOptions()
     #options.add_argument("--headless") 
-    #driver = webdriver.Chrome(options=options)
-    #driver = webdriver.Chrome()
-    print ('Browser started successfully. Navigating to the demo page to login.')
-    #driver.get("https://www.saucedemo.com/")
+    browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome()
+    
+    print('Browser started successfully. Navigating to the demo page to login.')
+    
+    browser.get("https://www.saucedemo.com/")
+    print(browser.title)
 
-    def get_item_input_box(login):
-      return login
+    browser.find_element(By.ID, 'user-name').send_keys("standard_user")
+    browser.find_element(By.ID, 'password').send_keys("secret_sauce")    
+    browser.find_element(By.ID, 'login-button').send_keys("button[class='submit-button btn_action']")
+    
+    print('test-User-logged-in:')
+    print('will be signed out in: 3 seconds.')
+    time.sleep(3)
 
 login('standard_user', 'secret_sauce')
-driver.close()
+def get_item_input_box(login):
+    return login
+
+time.sleep(5)
+print ('Finished: Log-out completed!')
+
